6904d16a5e2eb86de69175f17ba0eca60ec54dca
Caleb Caulk <calebcaulk02@outlook.com>
2025-02-22 18:34:06 +0100

    adding

---
+++ assignment-template-2025/my_code.py
@@ -0,0 +1,25 @@
+def initialize(arg1, arg2, ...):
+	""" high level description of what the function does
+	param arg1: description of argument 1
+	param arg2: description of argument 2
+	...
+	return: description of what the function returns
+	"""
+	
+	all_weights = ...
+	all_biases = ...
+	
+	
+	return all_weights, all_biases
+
+def compute_cost(arg1, arg2, ...):
+	""" high level description of what the function does
+	param arg1: description of argument 1
+	param arg2: description of argument 2
+	... 
+	return: description of what the function returns
+	"""
+	
+	L = 
+	
+	return L
